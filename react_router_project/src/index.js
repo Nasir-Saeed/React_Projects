7c5de180b99a9b37c818174f76e26973216594d8
Nasir Saeed <saeednasir483@gmail.com>
2024-07-01 11:33:45 +0500

    update files

--- react_router_project/src/index.js
+++ react_router_project/src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout';
+import { Home } from './components';
+import { About } from './components';
+import { Contact } from './components';
+import { Github } from './components';
+
+// First Method To Create Routes
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       },
+//       {
+//         path: "github",
+//         element: <Github />
+//       }
+//     ]
+//   }
+// ])
+
+// Second Method To Create Routes
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='github' element={<Github />} />
+    </Route>
+  )
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 

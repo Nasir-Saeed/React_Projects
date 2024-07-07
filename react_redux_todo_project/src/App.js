232d6725334bc48154a0879b43a526bf67c1890a
Nasir Saeed <saeednasir483@gmail.com>
2024-07-07 14:00:02 +0500

    added files

---
+++ react_redux_todo_project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AddTodo, Todos } from './components'
+
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;

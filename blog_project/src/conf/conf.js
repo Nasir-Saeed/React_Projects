57aa81c6a131ad5fd07fec333adb08a250c795d1
Nasir Saeed <saeednasir483@gmail.com>
2024-07-08 10:59:07 +0500

    added file

---
+++ blog_project/src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appWriteUrl: String(REACT_APP_APPWRITE_URL),
+    appwriteProjectId: String(REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(REACT_APP_APPWRITE_BUCKET_ID),
+}
+
+
+export default conf

e4e4c849baeb652425b095a97f8f60d3702b7265
Nasir Saeed <saeednasir483@gmail.com>
2024-07-09 17:18:08 +0500

    update files

--- blog_project/src/App.jsx
+++ blog_project/src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Footer, Header } from "./components";
 import { useDispatch } from 'react-redux'
-
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch();
 
-  return (
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logout(userData))
+        }
+      })
+      .finally(() => { setLoading(false) })
+    console.log("Use Effect Running")
+  }, [])
+
+
+  return !loading ? (
     <>
-      <Header />
-      <Footer />
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+          Todo:   {/* <Outlet/> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  );
+  ) : null
 }
 
 export default App;
--- blog_project/src/conf/conf.js
+++ blog_project/src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appWriteUrl: String(REACT_APP_APPWRITE_URL),
-    appwriteProjectId: String(REACT_APP_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(REACT_APP_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(REACT_APP_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(REACT_APP_APPWRITE_BUCKET_ID),
+    appWriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
 }
 
 

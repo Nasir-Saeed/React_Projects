eaeaa5f01dc28ae73cf4073b9a12472a490cf7a1
Nasir Saeed <saeednasir483@gmail.com>
2024-07-09 17:20:30 +0500

    update App.jsx

--- blog_project/src/App.jsx
+++ blog_project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Footer, Header } from "./components";
 import { useDispatch } from 'react-redux'
 import authService from "./appwrite/auth";
